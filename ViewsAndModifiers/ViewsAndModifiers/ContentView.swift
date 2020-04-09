a2866fff09cbca8331624e20b74b41a08970b21a
Naveen-Mouri <62740093+Naveen-Mouri@users.noreply.github.com>
2020-04-09 12:21:54 +0530

    initial commit

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Naveen Singisetty on 09/04/20.
+//  Copyright © 2020 MouriTech Pvt. Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
